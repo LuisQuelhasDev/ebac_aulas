bd794f46877e5cb5ca65ab229a31a46210087b48
LuisQuelhasDev <lf71@hotmail.com>
2025-05-26 11:12:25 -0300

    Atualizando arquivos

--- Modulo23/main.js
+++ Modulo23/main.js
@@ -20,4 +20,7 @@ document.addEventListener("DOMContentLoaded", function(){
         seguindo.innerText = json.following;
         link.href = json.html_url;
     })
+    .catch(function(erro){
+        alert("Algumas informações não foram cadastradas!")
+    })
 })
