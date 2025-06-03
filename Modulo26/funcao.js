9a9713a69591479e3eaf93c53fd21988645c4ce6
LuisQuelhasDev <lf71@hotmail.com>
2025-06-03 07:57:32 -0300

    Adicionando arquivos

---
+++ Modulo26/funcao.js
@@ -0,0 +1,10 @@
+function multiplica(n1, n2) {
+    return n1 * n2;
+}
+var calculo = multiplica(2, 2);
+console.log(calculo);
+function dizOlá(nome) {
+    return "Ol\u00E1 ".concat(nome);
+}
+var pessoa = dizOlá("Luis");
+console.log(pessoa);
---
+++ Modulo26/funcao.ts
@@ -0,0 +1,13 @@
+function multiplica(n1:number, n2:number):number{
+    return n1 * n2;
+}
+
+const calculo = multiplica(2,2)
+console.log(calculo)
+
+function dizOlá(nome:string):string{
+    return `Olá ${nome}`;
+}
+
+const pessoa = dizOlá("Luis");
+console.log(pessoa);
